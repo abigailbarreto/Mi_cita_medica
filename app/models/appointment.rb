32d95e6f4b576d97806cf06871262a03c85aa9b3
abigailbarreto <abigailbarreto1998@gmail.com>
2023-06-28 12:35:07 -0300

    linkeo

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,10 @@ class Appointment < ApplicationRecord
   belongs_to :medico
   belongs_to :user
   validates :meet_motive, presence: true
+  before_create :set_meet_state
+
+  private 
+  def set_meet_state
+    self.meet_state = true
+  end
 end
